4d314341180d3bee939d7d60981926a3cf1b556e
Raphael <raphaelndauwa@gmail.com>
2020-07-08 15:39:30 +0300

    setup springboot application

---
+++ src/test/java/com/example/codebbq/CodebbqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codebbq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodebbqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
